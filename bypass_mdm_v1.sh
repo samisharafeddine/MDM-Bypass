3b7b9abb7ba00f9889498894dae320b7f14f95b5
Sami Sharafeddine <samisharafeddine@gmail.com>
2023-10-09 23:14:38 +0300

    Update bypass_mdm_v1.sh

--- bypass_mdm_v1.sh
+++ bypass_mdm_v1.sh
@@ -15,6 +15,9 @@ select opt in "${options[@]}"; do
     case $opt in
     "Bypass MDM from Recovery")
         echo -e "${GRN}Bypass from Recovery"
+        if [ -d "/Volumes/Macintosh HD - Data" ]; then
+               diskutil rename "Macintosh HD - Data" "Data"
+        fi
         echo -e "${GRN}Create a new user"
         echo -e "${BLU}Press Enter to continue, Note: Leaving it blank will default to the automatic user (Apple)"
           echo -e "Enter Real Name (Default: Apple)"
@@ -26,7 +29,7 @@ select opt in "${options[@]}"; do
           echo -e "${BLUE}Enter the password (default: 1234)"
         read passw
           passw="${passw:=1234}"
-        dscl_path='/Volumes/Macintosh\ HD\ -\ Data/private/var/db/dslocal/nodes/Default'
+        dscl_path='/Volumes/Data/private/var/db/dslocal/nodes/Default'
         echo -e "${GREEN}Creating User"
           # Create user
         dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username"
@@ -35,7 +38,7 @@ select opt in "${options[@]}"; do
          dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" RealName "$realName"
         dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" UniqueID "501"
         dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" PrimaryGroupID "20"
-        mkdir "/Volumes/Macintosh\ HD\ -\ Data/Users/$username"
+        mkdir "/Volumes/Data/Users/$username"
         dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" NFSHomeDirectory "/Users/$username"
         dscl -f "$dscl_path" localhost -passwd "/Local/Default/Users/$username" "$passw"
         dscl -f "$dscl_path" localhost -append "/Local/Default/Groups/admin" GroupMembership $username
@@ -44,7 +47,7 @@ select opt in "${options[@]}"; do
         echo "0.0.0.0 iprofiles.apple.com" >>/Volumes/Macintosh\ HD/etc/hosts
         echo -e "${GREEN}Successfully blocked hosts"
         echo "Removing config profile"
-      touch /Volumes/Macintosh\ HD\ -\ Data/private/var/db/.AppleSetupDone
+      touch /Volumes/Data/private/var/db/.AppleSetupDone
         rm -rf /Volumes/Macintosh\ HD/var/db/ConfigurationProfiles/Settings/.cloudConfigHasActivationRecord
     rm -rf /Volumes/Macintosh\ HD/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordFound
     touch /Volumes/Macintosh\ HD/var/db/ConfigurationProfiles/Settings/.cloudConfigProfileInstalled
